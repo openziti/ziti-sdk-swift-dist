e0b99bbcd13f83f61a46dd255eddb626cd08afc0
ziti-ci <ziti-ci@netfoundry.io>
2026-06-10 17:22:18 +0000

    set version = 0.41.54; set checksum = f91369632873e169a74dafda4033df66e29972d5d862a045b6e60c93107b060f

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let version = "0.41.53"
-let checksum = "187665161c7389715f1d6f6fe9b27e9828071071d10e75633ab7f27a86810e69"
+let version = "0.41.54"
+let checksum = "f91369632873e169a74dafda4033df66e29972d5d862a045b6e60c93107b060f"
 
 let package = Package(
     name: "CZiti",
